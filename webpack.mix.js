fdbcd63a26c610e381e0710b4c822e06d0c675b7
Oliver Davies <oliver@oliverdavies.uk>
2019-05-16 02:51:25 +0100

    Add PurgeCSS
    From 513K to 5K! :)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,18 @@
 const mix = require('laravel-mix')
+const glob = require('glob-all')
+
+require('laravel-mix-purgecss')
 
 mix.disableNotifications()
  .postCss('src/css/app.css', 'dist/css', [
     require('tailwindcss')(),
     require('postcss-nested')()
   ])
+ .purgeCss({
+    paths: () => glob.sync([
+      path.join(__dirname, 'index.html'),
+    ]),
+ })
   // .browserSync({
   //   proxy: 'localhost:8001',
   //   // files: ['dist/css/app.css', 'index.html__'],
